b8149e6160ade3ee64eb7b17cf62c9a0ff471c47
manelcolominas <manel.colominas@estudiantat.upc.edu>
2024-12-05 15:05:14 +0100

    Nova Versió del Client

--- Battleship/Form2.Designer.cs
+++ Battleship/Form2.Designer.cs
@@ -28,63 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.user_dataGridView = new System.Windows.Forms.DataGridView();
+            this.opponent_dataGridView = new System.Windows.Forms.DataGridView();
+            this.form2_shoot_button = new System.Windows.Forms.Button();
+            this.form2_send_fleet_position_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.user_dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opponent_dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // user_dataGridView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(296, 675);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 92;
-            this.dataGridView1.RowTemplate.Height = 37;
-            this.dataGridView1.Size = new System.Drawing.Size(587, 531);
-            this.dataGridView1.TabIndex = 0;
+            this.user_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.user_dataGridView.Location = new System.Drawing.Point(153, 854);
+            this.user_dataGridView.Name = "user_dataGridView";
+            this.user_dataGridView.RowHeadersWidth = 92;
+            this.user_dataGridView.RowTemplate.Height = 37;
+            this.user_dataGridView.Size = new System.Drawing.Size(874, 724);
+            this.user_dataGridView.TabIndex = 0;
+            this.user_dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.user_dataGridView_CellClick);
+            this.user_dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.user_dataGridView_CellContentClick);
             // 
-            // dataGridView2
+            // opponent_dataGridView
             // 
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(296, 37);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersWidth = 92;
-            this.dataGridView2.RowTemplate.Height = 37;
-            this.dataGridView2.Size = new System.Drawing.Size(587, 531);
-            this.dataGridView2.TabIndex = 1;
+            this.opponent_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.opponent_dataGridView.Location = new System.Drawing.Point(153, 37);
+            this.opponent_dataGridView.Name = "opponent_dataGridView";
+            this.opponent_dataGridView.RowHeadersWidth = 92;
+            this.opponent_dataGridView.RowTemplate.Height = 37;
+            this.opponent_dataGridView.Size = new System.Drawing.Size(874, 735);
+            this.opponent_dataGridView.TabIndex = 1;
+            this.opponent_dataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.opponent_dataGridView_CellClick);
             // 
-            // button1
+            // form2_shoot_button
             // 
-            this.button1.Location = new System.Drawing.Point(977, 687);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(262, 95);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Shoot";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.form2_shoot_button.Location = new System.Drawing.Point(1357, 258);
+            this.form2_shoot_button.Name = "form2_shoot_button";
+            this.form2_shoot_button.Size = new System.Drawing.Size(335, 95);
+            this.form2_shoot_button.TabIndex = 2;
+            this.form2_shoot_button.Text = "Shoot";
+            this.form2_shoot_button.UseVisualStyleBackColor = true;
+            this.form2_shoot_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // form2_send_fleet_position_button
+            // 
+            this.form2_send_fleet_position_button.Location = new System.Drawing.Point(1419, 718);
+            this.form2_send_fleet_position_button.Name = "form2_send_fleet_position_button";
+            this.form2_send_fleet_position_button.Size = new System.Drawing.Size(309, 112);
+            this.form2_send_fleet_position_button.TabIndex = 3;
+            this.form2_send_fleet_position_button.Text = "Send Fleet Position";
+            this.form2_send_fleet_position_button.UseVisualStyleBackColor = true;
+            this.form2_send_fleet_position_button.Click += new System.EventHandler(this.form2_send_fleet_position_button_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1701, 1310);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1954, 1590);
+            this.Controls.Add(this.form2_send_fleet_position_button);
+            this.Controls.Add(this.form2_shoot_button);
+            this.Controls.Add(this.opponent_dataGridView);
+            this.Controls.Add(this.user_dataGridView);
             this.Name = "Form2";
             this.Text = "Form2";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.user_dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opponent_dataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView user_dataGridView;
+        private System.Windows.Forms.DataGridView opponent_dataGridView;
+        private System.Windows.Forms.Button form2_shoot_button;
+        private System.Windows.Forms.Button form2_send_fleet_position_button;
     }
 }
--- Battleship/Form2.cs
+++ Battleship/Form2.cs
@@ -12,14 +12,135 @@ namespace Battleship
 {
     public partial class Form2 : Form
     {
+        private List<(int Row, int Column)> userselectedCells = new List<(int, int)>();
+        private List<(int Row, int Column)> opponentselectedCells = new List<(int, int)>();
+
         public Form2()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            form2_shoot_button.Visible= false;
+            form2_send_fleet_position_button.Visible = true;
+            opponent_dataGridView.CellClick -= opponent_dataGridView_CellClick;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            ConfigureTable(user_dataGridView);
+            ConfigureTable(opponent_dataGridView);
+        }
+        private void form2_send_fleet_position_button_Click(object sender, EventArgs e)
+        {
+            form2_send_fleet_position_button.Visible= false;
+
+            form2_shoot_button.Visible = true;
+            user_dataGridView.CellClick -= user_dataGridView_CellClick;
+            opponent_dataGridView.CellClick += opponent_dataGridView_CellClick;
+
+            // Genera un mensaje con las coordenadas seleccionadas
+            string message = "Celdas seleccionadas:\n";
+            foreach (var (Row, Column) in userselectedCells)
+            {
+                message += $"Fila: {Row}, Columna: {Column}\n";
+            }
+
+            // Muestra las coordenadas en un MessageBox
+            MessageBox.Show(message, "Celdas Seleccionadas");
+    }
+
+        private void opponent_dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var dgv = sender as DataGridView;
+            var cell = dgv.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var cellPosition = (Row: e.RowIndex, Column: e.ColumnIndex);
+            dgv.InvalidateCell(cell);
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                if (opponentselectedCells.Contains(cellPosition))
+                {
+                    cell.Style.BackColor = Color.White;
+                    opponentselectedCells.Remove(cellPosition);
+                }
+                else
+                {
+                    if (opponentselectedCells.Count >= 1 && !opponentselectedCells.Contains(cellPosition))
+                    {
+                        MessageBox.Show("Solo puedes seleccionar hasta 1 celda.", "Límite alcanzado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        cell.Style.BackColor = Color.Red;
+                        opponentselectedCells.Add(cellPosition);
+                    }
+                }
+                dgv.InvalidateCell(cell);
+            }
+        }
+
+        private void user_dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // no eliminar la funció
+        }
+        private void ConfigureTable(DataGridView dgv)
+        {
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            dgv.GridColor = Color.Black;
+            dgv.DefaultCellStyle.SelectionBackColor = Color.White;
+            dgv.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dgv.RowHeadersVisible = true;  // Ocultar encabezados de fila si no se necesitan
+
+            // Configura 10 columnas
+            dgv.ColumnCount = 10;
+            for (int i = 0; i < 10; i++)
+            {
+                dgv.Columns[i].Name = $"{i + 1}";
+                dgv.Columns[i].Width = 30;  // Ancho de las columnas
+            }
+
+            // Configura 10 filas con letras de A a J
+            string[] rowNames = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+            dgv.RowCount = 10;
+            for (int i = 0; i < 10; i++)
+            {
+                dgv.Rows[i].HeaderCell.Value = rowNames[i];  // Asigna la letra a cada fila
+                dgv.RowHeadersWidth = 50;
+                dgv.Rows[i].Height = 30;  // Altura de las filas
+            }
+        }
+
+        private void user_dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var dgv = sender as DataGridView;
+            var cell = dgv.Rows[e.RowIndex].Cells[e.ColumnIndex];
+            var cellPosition = (Row: e.RowIndex, Column: e.ColumnIndex);
+            dgv.InvalidateCell(cell);
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                if (userselectedCells.Contains(cellPosition))
+                {
+                    cell.Style.BackColor = Color.White;
+                    userselectedCells.Remove(cellPosition);
+                }
+                else
+                {
+                    if (userselectedCells.Count >= 10 && !userselectedCells.Contains(cellPosition))
+                    {
+                        MessageBox.Show("Solo puedes seleccionar hasta 10 celdas.", "Límite alcanzado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        cell.Style.BackColor = Color.Black;
+                        userselectedCells.Add(cellPosition);
+                    }
+                }
+                dgv.InvalidateCell(cell);
+                dgv.InvalidateCell(cell);
+            }
         }
     }
 }
